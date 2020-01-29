993bccba010da94e13acd5178abb9790255674c5
isys35 <isys35@mail.ru>
2020-01-29 13:40:44 +0300

    Изменение запроса по очистке дубликатов Добавлена очистка дубликатов в таблице bet

--- clear_db.py
+++ clear_db.py
@@ -3,12 +3,14 @@ import sqlite3
 from collections import Counter
 import statistics
 
+db = 'oddsportal.db'
 
-def clear_duplicate():
-    db = 'oddsportal.db'
+
+def clear_duplicate_game():
     con = sqlite3.connect(db)
     cur = con.cursor()
     query = 'SELECT url FROM game'
+    print('[INFO] Подсчёт дубликатов игр')
     cur.execute(query)
     urls = [url[0] for url in cur.fetchall()]
     counter_urls = Counter(urls)
@@ -17,58 +19,65 @@ def clear_duplicate():
         if item > 1:
             url_duplicate_list.append(key)
     duplicates = len(url_duplicate_list)
-    print('[INFO] Количество дубликатов: ' + str(duplicates))
-    time_list = []
-    ids_list = []
-    for url in url_duplicate_list:
-        start = time.time()
-        query = 'SELECT id FROM game WHERE url = ?'
-        cur.execute(query, [url])
-        ids = [index[0] for index in cur.fetchall()]
-        ids_list += ids[1:]
-        duplicates -= 1
-        print('[INFO] Количество дубликатов: ' + str(duplicates))
-        end = time.time()
-        final_time = end - start
-        time_list.append(final_time)
-        time_to_compl = statistics.mean(time_list) * duplicates
-        hour = int(time_to_compl // 3600)
-        minute = int((time_to_compl % 3600) // 60)
-        second = (time_to_compl % 3600) % 60
-        print('[INFO] Осталось примерно {} часов {} минут {} секунд'.format(hour, minute, second))
-    print('[INFO] Удаляем дубликаты...')
-    if len(ids_list) == 1:
-        query_list = '(%i)' % ids_list[0]
-    else:
-        query_list = tuple(ids_list)
-    query = 'DELETE FROM game WHERE id IN {}'.format(query_list)
-    cur.execute(query)
-    query = 'DELETE FROM bet WHERE game_id IN {}'.format(query_list)
-    cur.execute(query)
-    query = 'DELETE FROM game_info WHERE game_id IN {}'.format(query_list)
-    cur.execute(query)
-    con.commit()
-    print('[INFO] Дубликаты удалены.')
-    print('[INFO] Создаём новую таблицу')
-    query = """CREATE TABLE `game_new` (`id`	INTEGER PRIMARY KEY AUTOINCREMENT,`command1` TEXT, `command2` TEXT, 
-    `url` TEXT UNIQUE, `date`	TEXT, `timematch`	TEXT, `result`	TEXT, `sport`	TEXT, `country`	TEXT, 
-    `liga`	TEXT) """
-    cur.execute(query)
-    print('[INFO] Копируем данные из старой таблицы')
-    query = """INSERT INTO `game_new` SELECT * FROM `game`"""
-    cur.execute(query)
-    print('[INFO] Удаляем старую таблицу')
-    query = """DROP TABLE `game`"""
-    cur.execute(query)
-    print('[INFO] Переименовываем новую таблицу')
-    query = """ALTER TABLE `game_new` RENAME TO game;"""
-    cur.execute(query)
-    con.commit()
-    con.execute("VACUUM")
+    print('[INFO] Количество дубликатов игр: ' + str(duplicates))
+    if duplicates:
+        print('[INFO ]Удаляеем дубликаты игр')
+        query = 'DELETE FROM bet WHERE game_id NOT IN (SELECT min(id) FROM game GROUP BY url)'
+        cur.execute(query)
+        query = 'DELETE FROM game_info WHERE game_id NOT IN (SELECT min(id) FROM game GROUP BY url)'
+        cur.execute(query)
+        query = 'DELETE FROM game WHERE rowid NOT IN (SELECT min(rowid) FROM game GROUP BY url)'
+        cur.execute(query)
+        con.commit()
+        print('[INFO] Дубликаты удалены.')
+        print('[INFO] Создаём новую таблицу')
+        query = """CREATE TABLE `game_new` (`id`	INTEGER PRIMARY KEY AUTOINCREMENT,`command1` TEXT, `command2` TEXT, 
+        `url` TEXT UNIQUE, `date`	TEXT, `timematch`	TEXT, `result`	TEXT, `sport`	TEXT, `country`	TEXT, 
+        `liga`	TEXT) """
+        cur.execute(query)
+        print('[INFO] Копируем данные из старой таблицы')
+        query = """INSERT INTO `game_new` SELECT * FROM `game`"""
+        cur.execute(query)
+        print('[INFO] Удаляем старую таблицу')
+        query = """DROP TABLE `game`"""
+        cur.execute(query)
+        print('[INFO] Переименовываем новую таблицу')
+        query = """ALTER TABLE `game_new` RENAME TO game;"""
+        cur.execute(query)
+        con.commit()
+        con.execute("VACUUM")
+        con.commit()
     cur.close()
     con.close()
-    print('[INFO] Завершено.')
+
+
+def clear_duplicate_bet():
+    con = sqlite3.connect(db)
+    cur = con.cursor()
+    print('[INFO] Подсчёт дубликатов ставок')
+    query = 'SELECT bookmaker_id,game_id FROM bet'
+    cur.execute(query)
+    bets = [(bet[0], bet[1]) for bet in cur.fetchall()]
+    counter_bets = Counter(bets)
+    bets_duplicate_list = []
+    for key, item in counter_bets.items():
+        if item > 1:
+            bets_duplicate_list.append(key)
+    duplicates = len(bets_duplicate_list)
+    print('[INFO] Количество дубликатов коэф: ' + str(duplicates))
+    if duplicates:
+        print('Удаляеем дубликаты ставок')
+        query = 'DELETE FROM bet WHERE rowid NOT IN (SELECT min(rowid) FROM bet GROUP BY bookmaker_id, game_id)'
+        cur.execute(query)
+        con.commit()
+        con.execute("VACUUM")
+        con.commit()
+    cur.close()
+    con.close()
+
 
 
 if __name__ == "__main__":
-    clear_duplicate()
+    clear_duplicate_game()
+    clear_duplicate_bet()
+    print('[INFO] Завершено.')

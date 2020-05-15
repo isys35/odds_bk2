529d870c50de1a7a241e7b66680719847bdc3ae9
isys35 <isys35@mail.ru>
2020-05-15 16:56:35 +0300

    получение id чемпионата

--- parser.py
+++ parser.py
@@ -7,9 +7,9 @@ from urllib.parse import unquote
 import time
 
 
-def save_page(page: str, file_name: str):
-    with open(file_name, 'w', encoding='utf8') as html:
-        html.write(page)
+def save_file(page: str, file_name: str):
+    with open(file_name, 'w', encoding='utf8') as file:
+        file.write(page)
 
 
 class Grabber:
@@ -43,7 +43,6 @@ class Grabber:
         28: 'MMA',
         30: 'Pesäpallo',
         36: 'eSports',
-
     }
     E = {
         1: [1, 2],
@@ -77,13 +76,68 @@ class Grabber:
         self.bookmakers = {}
         self.load_bookmakers()
 
+    def update_champs_id(self):
+        url = self.MAIN_URL + '/results/'
+        resp = requests.get(url, headers=self.HEADERS)
+        soup = BeautifulSoup(resp.text, 'lxml')
+        trs = soup.select('tr')
+        trs = [tr for tr in trs if 'xsid' in tr.attrs]
+        country = None
+        for tr in trs:
+            if 'class' in tr.attrs:
+                if tr['class'] == ['center']:
+                    country = tr.text.strip()
+            tds = tr.select('td')
+            for td in tds:
+                a = td.select_one('a')
+                if a:
+                    if a['href'][:2] != '//':
+                        url_champ = self.MAIN_URL + a['href']
+                        resp_champ = requests.get(url_champ, headers=self.HEADERS)
+                        if 'Page not found' in resp_champ.text:
+                            continue
+                        sport_id = int(tr['xsid'])
+                        data_champ = self.get_data_script(resp_champ.text, 'Tournament')
+                        champ_text_id = data_champ['id']
+                        request_time = int(time.time() * 1000)
+                        url_champ_ajax = f"https://fb.oddsportal.com/ajax-sport-country-tournament-archive/{sport_id}/{champ_text_id}/X0/1/0/1/?_={request_time}"
+                        print(url_champ_ajax)
+                        # print(data_champ)
+                        # print(tr['xsid'])
+                        # print(country)
+                        # print(td.text)
+
+
+    @staticmethod
+    def get_data_script(page: str, key: str):
+        soup = BeautifulSoup(page, 'lxml')
+        script = soup.select_one('script:contains("new OpHandler")').text
+        json_text = re.search(f'Page{key}\((.*?)\);', script)
+        if not json_text:
+            print('script not found')
+            sys.exit()
+        try:
+            json_data = json.loads(json_text.group(1))
+        except ValueError:
+            print('json not parsed')
+            sys.exit()
+        return json_data
+
+    def get_data_ajax(self, resp: str):
+        json_text = re.search('globals\.jsonpCallback\(.*, (.*)\);', resp)
+        json_text = json_text.group(1)
+        json_text = re.sub('null', 'None', json_text)
+        json_text = re.sub('true', 'True', json_text)
+        json_text = re.sub('false', 'False', json_text)
+        return eval(json_text)
+
     def load_bookmakers(self):
         with open("bookmakersData.json", "r") as read_file:
             bookmakers_data = json.load(read_file)
             for bookmaker in bookmakers_data:
                 self.bookmakers[bookmaker] = bookmakers_data[bookmaker]['WebName']
 
-    def get_match_data(self, url):
+    def get_match_data(self, url: str):
         resp = requests.get(url, headers=self.HEADERS)
         soup = BeautifulSoup(resp.text, 'lxml')
         game_info_soup = soup.select_one('#col-content')
@@ -95,16 +149,9 @@ class Grabber:
             result = result_live.text
         else:
             result = None
-        script = soup.select_one('script:contains("new OpHandler")').text
-        json_text = re.search('PageEvent\((.*?)\);', script)
-        if not json_text:
-            print('script not found')
-            sys.exit()
-        try:
-            json_data = json.loads(json_text.group(1))
-        except ValueError:
-            print('json not parsed')
-            sys.exit()
+        json_data = self.get_data_script(resp.text, 'Event')
+        command1 = json_data['home']
+        command2 = json_data['away']
         version_id = json_data['versionId']
         sport_id = json_data['sportId']
         id = json_data['id']
@@ -116,14 +163,10 @@ class Grabber:
         headers_fb = self.HEADERS
         headers_fb['Referer'] = url
         resp = requests.get(url, headers=headers_fb)
-        json_text = re.search('globals\.jsonpCallback\(.*, (.*)\);', resp.text)
-        json_text = json_text.group(1)
-        json_text = re.sub('null', 'None', json_text)
-        json_text = re.sub('true', 'True', json_text)
-        json_text = re.sub('false', 'False', json_text)
-        json_data = eval(json_text)
+        json_data = self.get_data_ajax(resp.text)
         openodds = json_data['d']['oddsdata']['back'][f'E-{e1}-{e2}-0-0-0']['opening_odds']
         openodds_filter = {}
+        print(json_data)
         for bookmaker_id in openodds:
             if bookmaker_id in self.bookmakers:
                 if type(openodds[bookmaker_id]) is dict:
@@ -138,7 +181,8 @@ class Grabber:
 
 if __name__ == '__main__':
     grabber = Grabber()
-    grabber.get_match_data('https://www.oddsportal.com/handball/africa/african-championship/egypt-tunisia-2uqxZtkE/')
+    grabber.update_champs_id()
+    #grabber.get_match_data('https://www.oddsportal.com/soccer/africa/africa-cup-of-nations/equatorial-guinea-tunisia-MujIJeiH/')
     # grabber.get_match_data('https://www.oddsportal.com/basketball/asia/asia-championship-u16/china-australia-bVGTfJ0e/')
 
 

3c88aa5e23b368b41ebfda563e62a2801ef0674f
isys35 <isys35@mail.ru>
2020-05-19 16:48:00 +0300

    write head in excel

--- excel_creator.py
+++ excel_creator.py
@@ -1,39 +1,58 @@
 from parser_odds import Grabber
 import xlwt, xlrd
+import subprocess
+import time
 
 
-def save_data_in_excel(data):
-    wb = xlwt.Workbook()
-    ws = wb.add_sheet('sheet')
-    style_horz_aligment = get_style('horz_aligment')
-    style_little_value = get_style('little_value')
-    style_average_value = get_style('average_value')
-    style_max_value = get_style('max_value')
-    style_min_value = get_style('min_value')
-    ws.col(0).width = 4000
-    if data['e1'] == 1:
-        len_cilumn_content = 10
-        ws.col(10).width = 6000
-        ws.col(12).width = 4000
-        ws.col(22).width = 6000
-    else:
-        len_cilumn_content = 7
-        ws.col(7).width = 6000
-        ws.col(9).width = 4000
-        ws.col(16).width = 4000
-    ws.col(0 + len_cilumn_content).width = 6000
-    write_head(data)
-    count_file = 0
-    while True:
-        try:
-            wb.save(f'info{count_file}.xls')
-            break
-        except PermissionError:
-            count_file += 1
+class ExcelWriter:
+    def __init__(self):
+        self.wb = xlwt.Workbook()
+        self.ws = self.wb.add_sheet('sheet')
+        self.style_horz_aligment = get_style('horz_aligment')
 
+    def save_data_in_excel(self, data):
+        style_little_value = get_style('little_value')
+        style_average_value = get_style('average_value')
+        style_max_value = get_style('max_value')
+        style_min_value = get_style('min_value')
+        self.ws.col(0).width = 4000
+        if data['e1'] == 1:
+            len_cilumn_content = 10
+        else:
+            len_cilumn_content = 7
+        self.ws.col(0 + len_cilumn_content).width = 6000
+        self.ws.col(2 + len_cilumn_content).width = 4000
+        self.ws.col(2 + len_cilumn_content * 2).width = 4000
+        self.write_head(data)
+        odds_data = data['open_odds']
+        odds_data_list_sort_time = list(odds_data.items())
+        odds_data_list_sort_time.sort(key=lambda i: i[1]['change_time'])
+        print(odds_data_list_sort_time)
+        count_file = 0
+        while True:
+            try:
+                self.wb.save(f'info{count_file}.xls')
+                break
+            except PermissionError:
+                count_file += 1
+        with subprocess.Popen(["start", "/WAIT", f'info{count_file}.xls'], shell=True) as doc:
+            doc.poll()
 
-def write_head(data):
-    pass
+    def write_head(self, data):
+        date = time.gmtime(data['date'])
+        self.ws.write(0, 0, data['sport'])
+        self.ws.write(0, 1, data['country'])
+        self.ws.write(0, 2, data['command1'])
+        self.ws.write(0, 3, data['command2'])
+        self.ws.write(1, 0, time.strftime('%d %B %Y', date))
+        self.ws.write(1, 1, time.strftime('%H:%M', date))
+        self.ws.write(2, 0, data['result'])
+        self.ws.write(5, 0, 'Букмекер')
+        self.ws.write(5, 1, 'П1', self.style_horz_aligment)
+        self.ws.write(5, 2, 'П2', self.style_horz_aligment)
+        if data['e1'] == 1:
+            self.ws.write(5, 3, 'П3', self.style_horz_aligment)
+
 
 
 def get_style(style_name):
@@ -61,7 +80,8 @@ def main():
     parser = Grabber()
     url_event = input('Введите ссылку:')
     data = parser.get_event_data(url_event)
-    save_data_in_excel(data)
+    excel = ExcelWriter()
+    excel.save_data_in_excel(data)
 
 
 if __name__ == '__main__':
